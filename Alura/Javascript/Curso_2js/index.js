2ab9d7e66685158c70f6ccd294978a1bc162c6d4
Miguel <miguelcarlosribeiro@hotmail.com>
2022-04-26 23:12:42 -0300

    Finalizado curso 2 js O.O

--- Alura/Javascript/Curso_2js/index.js
+++ Alura/Javascript/Curso_2js/index.js
@@ -1,26 +1,27 @@
 import { Cliente } from "./Cliente.js";
 import { Conta_corrente } from "./Conta_corrente.js";
 
+const cliente1 = new Cliente("Miguel", 11122233399);
 
-const cliente1 = new Cliente();
-cliente1.nome = "Miguel";
-cliente1.cpf = "11122233399";
+const cliente2 = new Cliente("arthur", 22233344455);
 
-const cliente2 = new Cliente();
-cliente2.nome = "arthur";
-cliente2.cpf = "22233344455";
+const conta_corrente_miguel = new Conta_corrente(1001, cliente1);
 
-const conta_corrente_miguel = new Conta_corrente();
-conta_corrente_miguel.agencia = 1001;
-conta_corrente_miguel.cliente = cliente1;
 conta_corrente_miguel.depositar(500);
 
+const conta2 = new Conta_corrente(102, cliente2);
 
-const conta2 = new Conta_corrente();
-conta2.cliente = cliente2;
-conta2.agencia = 102;
+let valor = 200;
 
-conta_corrente_miguel.transferir(200, conta2);
+conta_corrente_miguel.transferir(valor, conta2);
+
+console.log("Valor: ", valor);
+console.log(cliente1);
 
 console.log(conta_corrente_miguel);
-console.log(conta2);
+console.log(Conta_corrente.numero_de_contas);
+
+// Undefined e null
+//Undefined é diferente de null
+// null proposital
+//undefined pode ter esquecido de inserir o dado
